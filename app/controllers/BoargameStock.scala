181b01519668aaec3a623632aa14d352ac5766e3
davidhuttonlewis <d.huttonlewis@gmail.com>
2018-01-31 11:38:48 +0000

    have created and items detail page that displays information taken from a data set

--- app/controllers/BoargameStock.scala
+++ app/controllers/BoargameStock.scala
@@ -1,22 +1,21 @@
 package controllers
 
+import models.stockItem
 import play.api.mvc.{Action, Controller}
-
 class BoargameStock extends Controller{
 
-  val stock = Array(
-    ("Gloomhaven.jpg", "Gloomhaven","Gloomhaven is a game of Euro-inspired tactical combat in a persistent world of shifting motives", 140.00),
-    ("buffy.png", "Buffy the Vampire Slayer", "In Buffy the Vampire Slayer: The Board Game, you help Buffy and her friends defend the town of Sunnydale from an onslaught of vampires and demons",40.00)
 
-  )
+  val games = List(
+    stockItem("Gloomhaven","Gloomhaven.jpg", "Gloomhaven","Gloomhaven is a game of Euro-inspired tactical combat in a persistent world of shifting motives", 140.00),
+    stockItem("Buffy-the-Vampire-Slayer" ,"buffy.png", "Buffy the Vampire Slayer", "In Buffy the Vampire Slayer: The Board Game, you help Buffy and her friends defend the town of Sunnydale from an onslaught of vampires and demons",40.00))
 
   def boardgamePage = Action {
-    Ok(views.html.itemPage(stock))
+    Ok(views.html.itemPage(games))
   }
 
   def itemDetails(name: String) = Action {
 
-    Ok(views.html.itemPage(s"This pages is for $name"))
+    Ok(views.html.itemDetails(name,games))
 
   }
 
---
+++ app/models/stock.scala
@@ -0,0 +1,3 @@
+package models
+
+case class stockItem(id: String, image: String, name: String, info: String, price: Double )
